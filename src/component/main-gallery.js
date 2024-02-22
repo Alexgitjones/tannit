ea7e94a8246c5e261466efb6621fb5bc2f9c214a
ali raj <aliraj2.hashone@gmail.com>
2024-02-22 02:18:08 -0800

    gallery changed

--- src/component/main-gallery.js
+++ src/component/main-gallery.js
@@ -1,66 +1,108 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import LightGallery from 'lightgallery/react';
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgAutoplay from "lightgallery/plugins/autoplay";
+import lgVideo from "lightgallery/plugins/video";
 import "lightgallery/css/lightgallery.css";
 import "lightgallery/css/lg-zoom.css";
 import "lightgallery/css/lg-thumbnail.css";
 import "lightgallery/css/lg-autoplay.css";
-import "lightgallery/css/lg-share.css";
-import "lightgallery/css/lg-rotate.css";
+import "lightgallery/css/lg-video.css";
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
 
+    export default function  Mgallery  ()  {
+  const containerRef = useRef(null);
+  const [galleryContainer, setGalleryContainer] = useState(null);
 
-import  LightGallery  from "lightgallery/react/Lightgallery.es5";
+  const onInit = useCallback((detail) => {
+    if (detail) {
+      detail.instance.openGallery();
+    }
+  }, []);
 
-import lgthumbnail from "lightgallery/plugins/thumbnail";
-import lgZoom from "lightgallery/plugins/zoom";
-import lgAutoplay from "lightgallery/plugins/autoplay";
-import lgVideo from "lightgallery/plugins/video";
-import lgShare from "lightgallery/plugins/share";
-import lgRotate from "lightgallery/plugins/rotate";
+  useEffect(() => {
+    if (containerRef.current) {
+      setGalleryContainer('aaa');
+    }
+  }, []);
 
-export default function Mgallery(){
-    return(
-        <div>
-            <LightGallery
-            speed={500}
-            plugins={[lgthumbnail, lgZoom, lgAutoplay, lgVideo, lgShare, lgRotate]}>
-        <a href='assets/images/g4.png'>
-         <img
-          alt='demo'
-          src='assets/images/g4.png'
-          />
-       </a>
-        <a href='assets/images/g2.png'>
-         <img
-          alt='demo'
-          src='assets/images/g2.png'
-          />
-       </a>
-        <a href='assets/images/g3.png'>
-         <img
-          alt='demo'
-          src='assets/images/g3.png'
-          />
-       </a>
-        <a href='assets/images/g1.png'>
-         <img
-          alt='demo'
-          src='assets/images/g1.png'
-          />
-       </a>
-       
-        <a href='assets/images/g5.png'>
-         <img
-          alt='demo'
-          src='assets/images/g5.png'
-          />
-       </a>
-        <a href='assets/images/g6.png'>
-         <img
-          alt='demo'
-          src='assets/images/g6.png'
-          />
-       </a>
-            </LightGallery>
-        </div>
-    );
-}
-
+  return (
+    <div className="App">
+     
+      <div
+        style={{
+          height: '100vh',
+        }}
+        ref={containerRef}
+      ></div>
+      <div>
+        <LightGallery
+          container={containerRef.current}
+          onInit={onInit}
+          plugins={[lgZoom, lgThumbnail, lgAutoplay, lgVideo]}
+          closable={false}
+          showMaximizeIcon={true}
+          slideDelay={200}
+          thumbWidth={130}
+          thumbHeight={'100px'}
+          thumbMargin={16}
+          dynamic={true}
+          dynamicEl={[
+            {
+              src: 'assets/images/g4.png',
+              responsive:
+                'assets/images/g4.png',
+              thumb:
+                'assets/images/g4.png',
+            },
+            {
+              src: 'assets/images/g2.png',
+              responsive:
+                'assets/images/g2.png',
+              thumb:
+                'assets/images/g2.png',
+            },
+            {
+              src: 'assets/images/g3.png',
+              responsive:
+                'assets/images/g3.png',
+              thumb:
+                'assets/images/g3.png',
+            },
+            {
+              src: 'assets/images/g1.png',
+              responsive:
+                'assets/images/g1.png',
+              thumb:
+                'assets/images/g1.png',
+            },
+            {
+              src: 'assets/images/g5.png',
+              responsive:
+                'assets/images/g5.png',
+              thumb:
+                'assets/images/g5.png',
+            },
+            {
+              src: 'assets/images/g6.png',
+              responsive:
+                'assets/images/g6.png',
+              thumb:
+                'assets/images/g6.png',
+            },
+            {
+              src: 'assets/images/g6.png',
+              responsive:
+                'assets/images/g6.png',
+              thumb:
+                'assets/images/g6.png',
+            },
+            // Add more dynamic elements as needed
+          ]}
+          hash={false}
+          elementClassNames={'inline-gallery-container'}
+        ></LightGallery>
+      </div>
+    </div>
+  );
+};
